79b91b2a39c7b99f9f1ee7c54a80d7d3b27423ed
Xing Yang <x.yang89@yahoo.com>
2022-04-23 15:41:06 -0700

    Changed design concept

--- app.R
+++ app.R
@@ -1,13 +1,22 @@
 library(shiny)
-library(waiter)
+library(sass)
 
-options(shiny.maxRequestSize=200*1024^2)
+sass(
+  sass_file("www/app.scss"),
+  "www/app.css",
+  options = sass_options(
+    output_style = "compressed",
+    source_map_embed = TRUE
+  )
+)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "app.css"),
-    tags$link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"),
+    # tags$link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"),
+    # tags$link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/gh/octoshrimpy/bulma-o-steps@master/bulma-steps.css"),
+    tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"),
     tags$script(src = "app.js"),
     tags$script("
       Shiny.addCustomMessageHandler('remove_load_server', function(message) {
@@ -20,26 +29,83 @@ ui <- fluidPage(
       Shiny.addCustomMessageHandler('remove_load_submit', function(message) {
         $('#btn_submit').removeClass(message);})")
   ),
-  fluidRow(
+  fluidRow(class = "hero is-fullwidth",
+           tags$div(class = "hero-body",
+                    tags$p(class = "title", "Title"),
+                    tags$p(class = "subtitle", "Subtitle"))
+
+  ),
+  fluidRow(class = "pl-2 pr-2",
+    tags$ul(class = "steps has-gaps is-small has-content-centered is-balanced",
+            tags$li(class = "steps-segment is-active is-dashed",
+                    id = "step-model",
+                    tags$div(class ="steps-marker is-info is-hollow",
+                             id = "marker-model"),
+                    tags$div(class = "steps-content",
+                             "Model Vendor"
+                             )),
+            tags$li(class = "steps-segment is-dashed",
+                    id = "step-server",
+                    tags$div(class = "steps-marker",
+                             id = "marker-server"),
+                    tags$div(class = "steps-content",
+                             "Server")
+                    ),
+            tags$li(class = "steps-segment is-dashed",
+                    id = "step-database",
+                    tags$div(class ="steps-marker",
+                             id = "marker-database"),
+                    tags$div(class = "steps-content",
+                             "Database")
+                    ),
+            tags$li(class = "steps-segment is-dashed",
+                    tags$div(class = "steps-marker"),
+                    tags$div(class = "steps-content",
+                             "Portfolio")
+                    ),
+            tags$li(class = "steps-segment is-dashed",
+                    tags$div(class ="steps-marker"),
+                    tags$div(class = "steps-content",
+                             "Peril")
+                    ),
+            tags$li(class = "steps-segment is-dashed",
+                    id = "steps-bob",
+                    tags$div(class ="steps-marker"),
+                    tags$div(class = "steps-content",
+                             "Book of business")
+            ),
+            tags$li(class = "steps-segment is-dashed",
+                    id = "steps-date",
+                    tags$div(class = "steps-marker"),
+                    tags$div(class = "steps-content",
+                             "As of Date")
+            ),
+            tags$li(class = "steps-segment is-dashed",
+                    id = "steps-description",
+                    tags$div(class ="steps-marker"),
+                    tags$div(class = "steps-content",
+                             "Description")
+            ),
+            tags$li(class = "steps-segment is-dashed",
+                    tags$div(class = "steps-marker"),
+                    tags$div(class = "steps-content",
+                             "Submit")),
+
+  )),
+  fluidRow(class = "mt-4",
     column(width = 8, offset = 2,
            fluidRow(
-             tags$h4("Select a model vendor:")),
-           fluidRow(
-             column(width = 6,
-                    tags$button(class = "button is-small is-info is-outlined
-                                shiny-bound-input action-button",
-                                id = "btn_air", "AIR")),
-             column(width = 6,
-                    tags$button(class = "button is-small is-info is-outlined
-                                shiny-bound-input action-button",
-                                id = "btn_rms", "RMS"))
-             ),
+             tags$h4("Model vendor"),
+             tags$button(class = "button is-info is-outlined
+                         shiny-bound-input action-button",
+                         id = "btn_air", "AIR"),
+             tags$button(class = "button is-info is-outlined
+                       shiny-bound-input action-button",
+                       id = "btn_rms", "RMS")),
            fluidRow(
              tags$h4(id = "header_server",
                      class = "hidden",
-                     "Select a server:")
-           ),
-           fluidRow(
+                     "Server:"),
              tags$div(class = "select is-info is-fullwidth hidden",
                       id = "select_server_wrapper",
                       tags$select(
@@ -47,27 +113,76 @@ ui <- fluidPage(
                         tags$option(selected = TRUE, ""),
                         tags$option("Option 1"),
                         tags$option("Option 2")
-
                         )
                       )
-           ),
+             ),
            fluidRow(
              tags$h4(id = "header_database",
                      class = "hidden animate-flicker",
-                     "Loading the database...")
-           ),
-           fluidRow(
-             tags$div(class = "select is-info is-fullwidth is-loading hidden",
+                     "Loading the database..."),
+             tags$div(class = "select is-info is-fullwidth hidden",
                       id = "select_database_wrapper",
                       tags$select(
                         id = "select_database",
-                        disabled = TRUE,
-                        tags$option(selected = TRUE, "")
-                      )
+                        disabled = TRUE
+                        )
              )),
            fluidRow(
+             column(width = 6,
+                    class = "pl-0 pr-1",
+                    tags$h4(id = "header_portfolio",
+                            class = "hidden",
+                            "Portfolio"),
+                    tags$div(class = "select is-info is-fullwidth hidden",
+                             id = "select_portfolio_wrapper",
+                             tags$select(
+                               id = "select_portfolio",
+                               tags$option("Option 1"),
+                               tags$option("Option 2")
+                             ))
+                    ),
+             column(width = 6,
+                    class = "pl-0 pr-0",
+                    tags$h4(id = "header_peril",
+                            class = "hidden",
+                            "Peril"),
+                    tags$div(class = "select is-info is-fullwidth hidden",
+                             id = "select_peril_wrapper",
+                             tags$select(
+                               id = "select_peril",
+                               tags$option("Option 1"),
+                               tags$option("Option 2")
+                             ))
+             )),
+           fluidRow(
+             column(width = 6,
+                    class = "pl-0 pr-1",
+                    tags$h4(id = "header_bob",
+                            class = "hidden",
+                            "Book of business"),
+                    tags$div(class = "select is-info is-fullwidth hidden",
+                             id = "select_bob_wrapper",
+                             tags$select(
+                               id = "select_bob",
+                               tags$option("Option 1"),
+                               tags$option("Option 2")
+                             ))
+                    ),
+             column(width = 6,
+                    class = "pl-0 pr-0",
+                    tags$h4(id = "header_date",
+                            class = "hidden",
+                            "As Of Date"),
+                    tags$div(class = "hidden",
+                             dateInput(inputId = "select_date",
+                                       label = NULL)
+                             )
+                    )
+           ),
+
+           fluidRow(
              tags$button(class = "button is-fullwidth is-rounded is-outlined
-                         is-info shiny-bound-input action-button",
+                         is-info shiny-bound-input action-button hidden",
                          id = "btn_submit",
                          "Submit Dataset")
            )
@@ -84,7 +199,8 @@ server <- function(input, output, session) {
 
     Sys.sleep(3)
     updateSelectInput(inputId = "select_database",
-                      choices = c("Option 1", "Option 2"))
+                      choices = c("Option 1", "Option 2"),
+                      selected = character(0))
 
     session$sendCustomMessage(type = "remove_load_server",
                               message = "is-loading")
--- www/app.js
+++ www/app.js
@@ -1,7 +1,9 @@
  $(document).ready(function(){
 
+   var btn_model_click = 0;
    /* Change formatting of model vendor buttons */
   $("#btn_air").on("click", function() {
+     btn_model_click++;
 
      /* Change class of AIR button */
      $(this).removeClass("is-outlined");
@@ -10,9 +12,19 @@
      /* Change class of RMS button */
      $('#btn_rms').removeClass("is-active");
      $('#btn_rms').addClass("is-outlined");
+
+      /* Change the step formatting; tracks number of clicks to make sure this
+      only happens once*/
+      if (btn_model_click == 1) {
+         $('#step-model').removeClass("is-active is-dashed");
+         $('#marker-model.is-hollow').removeClass("is-hollow").append("<span class='icon'><i class='fa fa-check'></i></span>");
+         $('#step-server').addClass("is-active");
+         $('#marker-server').addClass("is-hollow is-info");
+      }
    });
 
   $("#btn_rms").on("click", function() {
+      btn_model_click++;
 
      /* Change class of AIR button */
      $(this).removeClass("is-outlined");
@@ -21,6 +33,15 @@
      /* Change class of RMS button */
      $('#btn_air').removeClass("is-active");
      $('#btn_air').addClass("is-outlined");
+
+      /* Change the step formatting; tracks number of clicks to make sure this
+      only happens once*/
+      if (btn_model_click == 1) {
+         $('#step-model').removeClass("is-active is-dashed");
+         $('#marker-model.is-hollow').removeClass("is-hollow").append("<span class='icon'><i class='fa fa-check'></i></span>");
+         $('#step-server').addClass("is-active");
+         $('#marker-server').addClass("is-hollow is-info");
+      }
    });
 
    /* Reveals DOM for server selection */
@@ -29,25 +50,48 @@
      $('#select_server_wrapper.hidden').removeClass("hidden").hide().fadeIn(1000);
    })
 
-    var change_count = 0
    /* Reveals DOM for database selection */
    $("#select_server").on("change", function() {
-     change_count++
 
      $('#header_database.hidden').removeClass("hidden").hide().fadeIn(1000);
      $('#select_database_wrapper.hidden').removeClass("hidden").hide().fadeIn(1000);
 
-     if (change_count > 1) {
-       $('#select_database_wrapper').addClass("is-loading");
-       $('#select_database').prop('disabled', true);
-       $('#header_database').addClass('animate-flicker');
-       $('#header_database').text('Loading the database...');
+      var server = $('#select_server option:selected').text();
+      if (server !== "") {
+         $('#select_database').prop('disabled', true);
+         $('#header_database').addClass('animate-flicker');
+         $('#header_database').text('Loading the database...');
+
+      /* Change the step formatting */
+         $('#step-server').removeClass("is-active is-dashed");
+         $('#marker-server.is-hollow').removeClass("is-hollow").append("<span class='icon'><i class='fa fa-check'></i></span>");
+         $('#step-database').addClass("is-active");
+         $('#marker-database').addClass("is-hollow is-info");
      };
    });
 
    $('#btn_submit').on('click', function() {
      $(this).addClass("is-loading")
-   })
+   });
+
+   $('#select_database').on("change", function() {
+      var database = $('#select_database option:selected').text();
+      if (database !== "") {
+         $('#header_portfolio.hidden').removeClass("hidden").hide().fadeIn(1000);
+         $('#select_portfolio_wrapper.hidden').removeClass("hidden").hide().fadeIn(1000);
+      }
+   });
+
+   $('#select_portfolio').on("change", function() {
+      $('#header_peril.hidden').removeClass("hidden").hide().fadeIn(1000);
+      $('#select_peril_wrapper.hidden').removeClass("hidden").hide().fadeIn(1000);
+   });
+
+   $('#select_peril').on("change", function() {
+      $('#header_bob.hidden').removeClass("hidden").hide().fadeIn(1000);
+      $('#select_bob_wrapper.hidden').removeClass("hidden").hide().fadeIn(1000);
+   });
+
  })
 
 
